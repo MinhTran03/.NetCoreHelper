21a831423a5a731c049c1636ffd7217e73fefc18
MinhTran <trancongminh503@gmail.com>
2021-04-18 10:43:15 +0700

    test

--- ConsoleMain/Program.cs
+++ ConsoleMain/Program.cs
@@ -1,24 +1,55 @@
-﻿using StopWatch;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ConsoleMain
 {
-   class Program
-   {
-      static void Main(string[] args)
-      {
-         Action action = () =>
-         {
-            for (int i = 0; i < 100; i++)
-            {
-               Console.WriteLine(i);
-            }
-         };
+    class Program
+    {
+        private static string _reportFontsFolderName = "ReportFonts";
 
-         var result = action.CalculateRunTime();
-         result.DisplayTime();
+        static async Task Main(string[] args)
+        {
+            //var privateFontCollection = new PrivateFontCollection();
+            //string webRootPath = @"D:\C# Advanced\DotNetCoreHelper\ConsoleMain";
 
-         Console.ReadKey();
-      }
-   }
+            //// get all font in directory ReportFont
+            //string pathToFontFolder = Path.Combine(webRootPath, _reportFontsFolderName);
+            //var filePaths = Directory.GetFiles(pathToFontFolder);
+            //foreach (var filePath in filePaths)
+            //{
+            //    privateFontCollection.AddFontFile(filePath);
+            //}
+            var t1 = TaskWithAsync();
+            //await t1;
+            //Console.WriteLine("after wait 5 seconds");
+
+            var t2 = TaskWithAsync2();
+            //await t2;
+            //Console.WriteLine("after wait 5 seconds 2");
+
+            await Task.WhenAll(t1, t2);
+            Console.WriteLine("done all {0} {1}", t1.Result[0], t2.Result[0]);
+
+            Console.ReadKey();
+        }
+
+        public static async Task<List<int>> TaskWithAsync()
+        {
+            Console.WriteLine("task 1 start");
+            await Task.Delay(2000);
+
+            return await Task.FromResult(new List<int>() { 1 });
+        }
+
+        public static async Task<Dictionary<int ,int>> TaskWithAsync2()
+        {
+            Console.WriteLine("task 2 start");
+            await Task .Delay(3000);
+
+            return await Task .FromResult(new Dictionary<int, int>() { [0] = 2 });
+        }
+    }
 }
---
+++ WebEnviroment/EnviromentHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+
+namespace WebEnviroment
+{
+    public class EnviromentHelper
+    {
+        public PrivateFontCollection FontCollection { get; set; }
+        public FontFamily[] FontFamilies { get => FontCollection.Families; }
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public EnviromentHelper(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            FontCollection = new PrivateFontCollection();
+            string webRootPath = _webHostEnvironment.WebRootPath;
+
+            // get all font in directory ReportFont
+            string path = Path.Combine(webRootPath, "ReportFonts");
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                FontCollection.AddFontFile(file);
+            }
+        }
+    }
+}
